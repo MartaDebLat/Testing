3684a4b3f210a692cf58752b468a8d0c8029de7a
MartaDebLat <marta.deblat@gmail.com>
2020-02-22 09:50:40 +0100

    Initial commit

---
+++ src/main/java/CalculatorTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CalculatorTest {
+    @Test
+    public void testNoOne (){
+        assertThat(1).isEqualTo(2);
+    }
+}
